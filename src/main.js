29727c304cdd3d93deec17730a4d186f08b10a77
murataka9 <me@murakamitakahito.com>
2020-08-03 13:10:26 +0900

    add BS init

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// New install
+// BootStrap
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css' 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGlobe, faBlog, faBirthdayCake, faImage } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faGithub, faHtml5, faCss3Alt, faJsSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+// VueTyper
 import VueTyperPlugin from 'vue-typer'
+
+// UseDeligate
 Vue.use(VueTyperPlugin)
+Vue.use(BootstrapVue)
 
 library.add(faGlobe, faBlog, faBirthdayCake, faImage, faTwitter, faGithub, faHtml5, faCss3Alt, faJsSquare)
 
