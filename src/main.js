4153f2f89d2a8130f0d02bd8116e5bc5e4dd5cfb
murataka9 <me@murakamitakahito.com>
2020-08-03 13:01:57 +0900

    add fontawesome and vue-typer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// New install
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGlobe, faBlog, faBirthdayCake, faImage } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faGithub, faHtml5, faCss3Alt, faJsSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import VueTyperPlugin from 'vue-typer'
+Vue.use(VueTyperPlugin)
+
+library.add(faGlobe, faBlog, faBirthdayCake, faImage, faTwitter, faGithub, faHtml5, faCss3Alt, faJsSquare)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
